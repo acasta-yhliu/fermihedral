452f31220ffd00cf6341558f76a14537eb4e0f9a
acasta-yhliu <yhliu2000@outlook.com>
2023-09-01 09:12:29 -0400

    changed startup config

--- fermihedral/__main__.py
+++ fermihedral/__main__.py
@@ -1,11 +1,16 @@
 import z3
 
 from .solver import DecentSolver
+from .model import MajoranaModel
 
 if __name__ == '__main__':
     # enable the parallel support, just try
     z3.set_param("parallel.enable", "true")
-    z3.set_param("timeout", 12000)
-    for i in range(1, 10):
-        solver = DecentSolver(i)
-        print(i, "=>", solver.solve())
+
+    model = MajoranaModel(10, 2 * 10 * 10, True)
+    print(model.solve())
+
+    # z3.set_param("timeout", 12000)
+    # for i in range(1, 10):
+    #     solver = DecentSolver(i)
+    #     print(i, "=>", solver.solve())
